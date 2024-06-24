805474d0959a4886f4d580bffae4b344c782542b
Fine <92whdtjs@gmail.com>
2024-06-24 16:38:46 +0900

    [#3] chore : Add Target for WatchOS on Existing Application Project

---
+++ AppleJuice/WatchAppleJuice Watch App/WatchAppleJuiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchAppleJuiceApp.swift
+//  WatchAppleJuice Watch App
+//
+//  Created by 이종선 on 6/24/24.
+//
+
+import SwiftUI
+
+@main
+struct WatchAppleJuice_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
