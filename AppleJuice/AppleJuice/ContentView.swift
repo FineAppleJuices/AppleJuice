7acb11004deab53050307cb189d1e98a9c2891df
Fine <92whdtjs@gmail.com>
2024-06-24 15:55:36 +0900

    [#1] chore : Initial Project Setting

---
+++ AppleJuice/AppleJuice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppleJuice
+//
+//  Created by 이종선 on 6/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
