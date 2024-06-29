ba0c501713c0e982cc2f05c0a4a02c98cb580ee6
fine <92whdtjs@gmail.com>
2024-06-29 11:37:42 +0900

    [#5] feat: InteractionType enum 추가
    각 상호작용별 애니메이션을 위한 frame을 가지고 있는 enum입니다.

--- AppleJuice/WatchAppleJuice Watch App/Model/InteractionType.swift
+++ AppleJuice/WatchAppleJuice Watch App/Model/InteractionType.swift
@@ -5,8 +5,24 @@
 //  Created by 이종선 on 6/28/24.
 //
 
-import Foundation
+import SwiftUI
 
-enum InteractionType {
+enum InteractionType: Identifiable, CaseIterable {
+    case shower
+    case tanning
+    case wipe
+    
+    var id: Self {self}
+    
+    var frameNames: [String] {
+        switch self {
+        case .shower:
+            return ["showerFrame1", "showerFrame2", "showerFrame3"]
+        case .tanning:
+            return ["tanningFrame1", "tanningFrame2", "tanningFrame3"]
+        case .wipe:
+            return ["wipeFrame1", "wipeFrame2", "wipeFrame3"] 
+        }
+    }
     
 }
