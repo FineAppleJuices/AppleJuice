aa31c5fa41463072b1a4d3eff3c9999426f80b52
norang00 <norang1221@gmail.com>
2024-09-18 19:19:13 +0900

    마지막 접속일자 저장할 때의 데이터 가공

--- AppleJuice/AppleJuice/AppleJuiceApp.swift
+++ AppleJuice/AppleJuice/AppleJuiceApp.swift
@@ -16,7 +16,7 @@ struct AppleJuiceApp: App {
         saveLastLoginDate() // 앱 실행 시 호출
         
     }
-
+    
     var body: some Scene {
         WindowGroup {
             GoalHistoryView()
@@ -26,8 +26,20 @@ struct AppleJuiceApp: App {
 
 // 앱이 시작될 때 마지막 접속 일자를 저장
 func saveLastLoginDate() {
-    let currentDate = Date() // 현재 날짜 가져오기
-    UserDefaults.standard.set(currentDate, forKey: "lastLoginDate") // UserDefaults에 저장
+    // 현재 날짜 가져오기
+    let currentDate = Date()
+    
+    // 컴포넌트 설정
+    let calendar = Calendar.current
+    var components = calendar.dateComponents([.year, .month, .day], from: currentDate)
+    components.hour = 0
+    components.minute = 0
+    
+    // 새로운 날짜 객체 생성 (시간을 00:00으로 설정)
+    let dateToSave = calendar.date(from: components)
+
+    // UserDefaults에 변환한 Date 저장
+    UserDefaults.standard.set(dateToSave!, forKey: "lastLoginDate")
 }
 
 // 마지막 접속 일자를 가지고 오기
